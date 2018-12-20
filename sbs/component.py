8e8c43a68d013cf289019767db67db94e3deb3e9
Omer Akram <om26er@gmail.com>
2018-12-21 00:31:36 +0500

    Fix crossbar/component startup

--- sbs/component.py
+++ sbs/component.py
@@ -1,16 +1,48 @@
-from autobahn.asyncio import wamp
+#
+# Copyright (C) 2018  Omer Akram
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+from autobahn.asyncio import component
 
 from sbs.controller import BrightnessControl
 
 
-class BrightnessComponent(wamp.ApplicationSession):
-    def __init__(self, config=None):
-        super().__init__(config)
-        self.controller = BrightnessControl(self)
+brightness_component = component.Component(
+    transports=[
+        {
+            "type": "websocket",
+            "url": "ws://localhost:5020/ws",
+            "endpoint": {
+                "type": "tcp",
+                "host": "localhost",
+                "port": 5020
+            }
+        }
+    ],
+    realm="realm1",
+)
 
-    async def onJoin(self, details):
-        reg = await self.register(self.controller.set_brightness, 'io.crossbar.set_brightness')
-        print("registered '{}'".format(reg.procedure))
 
-        reg2 = await self.register(self.controller.get_current_brightness_percentage, 'io.crossbar.get_brightness')
-        print("registered '{}'".format(reg2.procedure))
+@brightness_component.on_join
+async def register_procedures(session, details):
+    controller = BrightnessControl(session)
+
+    reg = await session.register(controller.set_brightness, 'io.crossbar.set_brightness')
+    print("registered '{}'".format(reg.procedure))
+
+    reg2 = await session.register(controller.get_current_brightness_percentage, 'io.crossbar.get_brightness')
+    print("registered '{}'".format(reg2.procedure))
