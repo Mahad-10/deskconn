42da91e2ebae84e2d153ad8a4e4b1432b9d2f849
Omer Akram <om26er@gmail.com>
2018-12-26 02:08:26 +0500

    Ship crossbar with the snap to simplify things

--- entry_point_helpers.py
+++ entry_point_helpers.py
@@ -1,4 +1,5 @@
 import os
+import socket
 
 
 def is_snap():
@@ -7,3 +8,28 @@ def is_snap():
 
 def get_cbdir():
     return os.environ.get('SNAP_USER_DATA') if is_snap() else 'crossbar-config'
+
+
+def stop_crossbar(main):
+    params = ['stop', '--cbdir']
+    cbdir = get_cbdir()
+
+    if os.path.exists(os.path.join(cbdir, 'node.pid')):
+        params.append(cbdir)
+        main(params)
+
+
+def get_local_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("www.google.com", 80))
+    res = s.getsockname()[0]
+    s.close()
+    return res
+
+
+def get_start_params():
+    params = ['start', '--cbdir', get_cbdir()]
+    if is_snap():
+        params.append('--config')
+        params.append(os.path.join(os.environ.get('SNAP'), 'crossbar-config/config.json'))
+    return params
