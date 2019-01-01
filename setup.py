9d4d8752996e9559f786a65286eb592a1ac1e66a
Omer Akram <om26er@gmail.com>
2019-01-01 16:07:33 +0500

    Move variables around

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ from setuptools import setup
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='screen-brightness-server',
+    name='deskconn',
     version='0.1.0',
     packages=['sbs'],
     url='https://github.com/om26er/screen-brightness-server',
     license='GNU GPL Version 3',
     author='Omer Akram',
     author_email='om26er@gmail.com',
-    description='WAMP server to expose desktop functionality over network.',
-    download_url='https://github.com/om26er/screen-brightness-server/tarball/0.1.0',
+    description='Expose your desktop functionalities over network.',
+    download_url='https://github.com/deskconn/deskconn-server/tarball/0.1.0',
     keywords=['linux', 'ubuntu'],
     install_requires=['autobahn[twisted,serialization]', 'crossbar', 'twisted', 'txaio', 'zeroconf', 'xlib'],
 )
