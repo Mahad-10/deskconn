5381ea7a64303ab2fc00db5327813bae93fa86af
Omer Akram <om26er@gmail.com>
2019-04-08 00:03:01 +0500

    Add config command to help setup

---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "
+To allow deskconn to run in the background, you need to create a systemd unit,
+which ensures that deskconn automatically starts whenever your computer is
+restarted.
+
+To do the above, create a file named deskconn.service in
+/home/$USER/.config/systemd/user/ (if that directory doesn't exist, create it)
+and paste the below config inside it (/home/$USER/.config/systemd/user/deskconn.service)
+
+[Unit]
+Description=Deskconn session service
+
+[Service]
+ExecStart=/snap/bin/deskconn.session
+
+[Install]
+WantedBy=default.target
+
+
+Then run below commands to enable that service
+
+systemctl --user enable deskconn.session
+systemctl --user start deskconn.session
+"
+
